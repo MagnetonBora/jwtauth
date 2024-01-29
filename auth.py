35542352a90f30d2b66fd9829052c1cc2cd701eb
Roman Dryndik <dryndikroman@gmail.com>
2024-01-29 23:08:26 +0100

    Implemented auth jwt token

---
+++ auth.py
@@ -0,0 +1,59 @@
+from datetime import datetime, timedelta
+from typing import Optional
+
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from starlette import status
+
+from db import get_db, User
+
+
+class Hash:
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    def verify_password(self, plain_password, hashed_password):
+        return self.pwd_context.verify(plain_password, hashed_password)
+
+    def get_password_hash(self, password: str):
+        return self.pwd_context.hash(password)
+
+
+SECRET_KEY = "secret_key"
+ALGORITHM = "HS256"
+DEFAULT_TOKEN_EXPIRE_SECONDS = 15 * 60
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+
+
+async def create_access_token(
+    data: dict,
+    expires_delta: Optional[float] = DEFAULT_TOKEN_EXPIRE_SECONDS
+):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(seconds=expires_delta)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email = payload["sub"]
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user: User = db.query(User).filter(User.email == email).first()
+    if user is None:
+        raise credentials_exception
+    return user
